b765f7ecc281edc3f405c66e7e3940ae6909c54f
unknown <estevan-mazo@hotmail.com>
2026-06-17 02:46:20 -0500

    feat: conectar componentes navbar y estudiantes en app.ts y planear estructura visual

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/shared/navbar/navbar';
+import { Estudiantes } from './components/features/estudiantes/estudiantes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar,
+  Estudiantes],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
