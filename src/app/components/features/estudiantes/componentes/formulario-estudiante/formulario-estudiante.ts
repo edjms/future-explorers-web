0d356b079931e5d73452cbdb263852302334996e
unknown <estevan-mazo@hotmail.com>
2026-06-21 07:34:24 -0500

    feat: implementar formulario reactivo con validaciones para registro de estudiantes

--- src/app/components/features/estudiantes/componentes/formulario-estudiante/formulario-estudiante.ts
+++ src/app/components/features/estudiantes/componentes/formulario-estudiante/formulario-estudiante.ts
@@ -1,16 +1,32 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-estudiante',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario-estudiante.html',
   styles: ``,
 })
 export class FormularioEstudiante {
   @Output() cerrar = new EventEmitter<void>();
 
+  formularioEstudiante: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formularioEstudiante = this.fb.group({
+      nombre: ['', [  Validators.required, Validators.minLength(3)]], // Nombre obligatorio, mínimo 3 letras
+      correo: ['', [Validators.required, Validators.email]], // Correo obligatorio y con formato válido
+      estado: ['true', Validators.required], // Estado por defecto en 'Activo' (true)
+    });
+  }
   guardarEstudiante() {
-    alert('¡Guardando estudiante de prueba!');
-    this.cerrar.emit();
+    if (this.formularioEstudiante.valid) {
+      const datosEstudiante = this.formularioEstudiante.value;
+      console.log('🚀 DATOS LISTOS PARA SPRING BOOT:', datosEstudiante);
+      this.cerrar.emit();
+    } else {
+      alert('Por favor, rellena todos los campos correctamente.');
+    }
   }
 }
